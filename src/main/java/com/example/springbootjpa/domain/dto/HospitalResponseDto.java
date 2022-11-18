a176cc600b730b57ffbcee106515754ac9c60810
Senna97 <wkdtjgus0319@skuniv.ac.kr>
2022-11-18 13:08:28 +0900

    [add] HospitalResponseDto.java

---
+++ src/main/java/com/example/springbootjpa/domain/dto/HospitalResponseDto.java
@@ -0,0 +1,35 @@
+package com.example.springbootjpa.domain.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class HospitalResponseDto {
+    private int id;
+    private String roadNameAddress;
+    private String hospitalName;
+    private String businessTypeName;
+    private int patientRoomCount;
+    private int totalNumberOfBeds;
+    private float totalAreaSize;
+    private String businessStatusName;
+
+    public HospitalResponseDto(int id, String roadNameAddress, String hospitalName, String businessTypeName, int patientRoomCount, int totalNumberOfBeds, float totalAreaSize) {
+        this.id = id;
+        this.roadNameAddress = roadNameAddress;
+        this.hospitalName = hospitalName;
+        this.businessTypeName = businessTypeName;
+        this.patientRoomCount = patientRoomCount;
+        this.totalNumberOfBeds = totalNumberOfBeds;
+        this.totalAreaSize = totalAreaSize;
+    }
+
+    public void setBusinessStatusName(String businessStatusName) {
+        this.businessStatusName = businessStatusName;
+    }
+}
